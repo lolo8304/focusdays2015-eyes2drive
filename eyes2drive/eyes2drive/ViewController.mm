1a660e69feca411948bdbc6f2008b6885b647ba7
Lorenz Hänggi <lolo8304@gmail.com>
2015-07-08 04:25:34 +0200

    add CV camera features and start camera

--- eyes2drive/eyes2drive/ViewController.mm
+++ eyes2drive/eyes2drive/ViewController.mm
@@ -16,6 +16,7 @@
 
 #import "ViewController.h"
 #import <opencv2/videoio/cap_ios.h>
+#import <opencv2/imgproc/imgproc.hpp>
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
@@ -27,11 +28,38 @@
 
 @end
 
+
 @implementation ViewController
+
+- (IBAction)actionStart:(id)sender {
+    [self.videoCamera start];
+}
+
+#pragma mark - Protocol CvVideoCameraDelegate
+
+#ifdef __cplusplus
+// delegate method for processing image frames
+- (void)processImage:(cv::Mat&)image; {
+    // Do some OpenCV stuff with the image
+    cv::Mat image_copy;
+    cvtColor(image, image_copy, CV_BGRA2BGR);
+    
+    // invert image
+    bitwise_not(image_copy, image_copy);
+    cvtColor(image_copy, image, CV_BGR2BGRA);
+}
+#endif
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.videoCamera = [[CvVideoCamera alloc] initWithParentView:self.imageView];
+    self.videoCamera.delegate = self;
+    self.videoCamera.defaultAVCaptureDevicePosition = AVCaptureDevicePositionFront;
+    self.videoCamera.defaultAVCaptureSessionPreset = AVCaptureSessionPreset352x288;
+    self.videoCamera.defaultAVCaptureVideoOrientation = AVCaptureVideoOrientationPortrait;
+    self.videoCamera.defaultFPS = 30;
+    self.videoCamera.grayscaleMode = NO;
 }
 
 - (void)didReceiveMemoryWarning {
