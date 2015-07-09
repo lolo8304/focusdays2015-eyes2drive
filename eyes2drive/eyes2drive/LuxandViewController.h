a7fc17550cd5eb1c753b718d7411664e6ea123a5
Lorenz Hänggi <lolo8304@gmail.com>
2015-07-09 23:42:29 +0200

    add luxand view controller + 3rd party library

---
+++ eyes2drive/eyes2drive/LuxandViewController.h
@@ -0,0 +1,15 @@
+//
+//  LuxandViewController.h
+//  eyes2drive
+//
+//  Created by Lorenz Hänggi on 07/07/15.
+//  Copyright (c) 2015 Focusdays2015. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LuxandViewController : UIViewController
+
+
+@end
+
