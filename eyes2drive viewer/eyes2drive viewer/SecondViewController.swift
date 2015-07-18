31b80171f5721fd3cb1331b01bb0877f149cfa82
Lorenz Hänggi <lolo8304@gmail.com>
2015-07-18 08:14:09 +0200

    split code and create "eyes2drive" and "eyes2drive viewer" because notifications are not shown on watch if iPhone is NOT locked showcase must be done with 2 iPhone + 1 watch. 1 iPhone + app with "eyes2drive" camera and 1 iPhone with "eyes2drive" viewer and watch app

---
+++ eyes2drive viewer/eyes2drive viewer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  eyes2drive viewer
+//
+//  Created by Lorenz Hänggi on 18/07/15.
+//  Copyright (c) 2015 Focusdays2015. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ eyes2drive viewer/eyes2drive viewerTests/eyes2drive_viewerTests.swift
@@ -0,0 +1,36 @@
+//
+//  eyes2drive_viewerTests.swift
+//  eyes2drive viewerTests
+//
+//  Created by Lorenz Hänggi on 18/07/15.
+//  Copyright (c) 2015 Focusdays2015. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class eyes2drive_viewerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
