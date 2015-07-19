df9ba22969d5d0950fa6de057062a80726c54a09
Lorenz Hänggi <lolo8304@gmail.com>
2015-07-19 03:32:29 +0200

    add Async as pod to run threads in background with Swift add SwiftyJSON swift code to have simple access to JSON data prepare for further pods

--- eyes2drive viewer/eyes2drive viewer/AppDelegate.swift
+++ eyes2drive viewer/eyes2drive viewer/AppDelegate.swift
@@ -5,17 +5,61 @@
 //  Created by Lorenz Hänggi on 18/07/15.
 //  Copyright (c) 2015 Focusdays2015. All rights reserved.
 //
+// installing cocoapads: https://guides.cocoapods.org/using/using-cocoapods.html
+// installing Async: 
+//      pod https://github.com/duemunk/async or
+// using NSTimer http://stackoverflow.com/questions/25951980/swift-do-something-every-x-minutes
+// run HTTP call asynchronous http://stackoverflow.com/questions/24016142/how-to-make-an-http-request-in-swift
+// JSON with Swift:
+//      http://www.raywenderlich.com/82706/working-with-json-in-swift-tutorial
+//      https://github.com/lingoer/SwiftyJSON / Pod does not work, copy paste and fixed for Swift 1.2
 
 import UIKit
+import Foundation
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var updateEvents: NSTimer?
+    var updateEventsRunning: Bool = false
+    
+    
+    
+    func updateEventsFromServer()
+    {
+        if (!self.updateEventsRunning) {
+            self.updateEventsRunning = true;
+            NSLog("process 'updateEvents' reserved")
+            
+            let url = NSURL(string: "http://tomcat7-focusdays2015.rhcloud.com/rest/store/objects")
+            let request = NSURLRequest(URL: url!)
+            NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {(response, data, error) in
+
+                //print out full data string for debug
+                println(NSString(data: data, encoding: NSUTF8StringEncoding))
+                
+                //use SwiftyJSON
+                let json = JSON(data: data)
+                if let userName = json[0]["name"].string {
+                    println(userName)
+                }
+                
+                self.updateEventsRunning = false
+                NSLog("process 'updateEvents' released")
+            }
+            
+        } else {
+            NSLog("process 'updateEvents' is still running")
+        }
+    }
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        self.updateEvents = NSTimer.scheduledTimerWithTimeInterval(6.0, target: self, selector: Selector("updateEventsFromServer"), userInfo: nil, repeats: true)
+        
         return true
     }
 
@@ -39,6 +83,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        updateEvents?.invalidate()
     }
 
 
--- eyes2drive viewer/eyes2drive viewer/FirstViewController.swift
+++ eyes2drive viewer/eyes2drive viewer/FirstViewController.swift
@@ -5,21 +5,24 @@
 //  Created by Lorenz Hänggi on 18/07/15.
 //  Copyright (c) 2015 Focusdays2015. All rights reserved.
 //
+// get appDelegate: http://stackoverflow.com/questions/24046164/how-do-i-get-a-reference-to-the-app-delegate-in-swift
 
 import UIKit
 
+
 class FirstViewController: UIViewController {
 
+    var appDelegate: AppDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
