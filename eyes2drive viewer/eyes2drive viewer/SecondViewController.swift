585119c0f3d27145c3c9575d2c55586e5c4b8fc6
Rémy Schumm <dev@schumm.ch>
2015-09-10 15:55:53 +0200

    start/stop verdrahtet

--- eyes2drive viewer/eyes2drive viewer/SecondViewController.swift
+++ eyes2drive viewer/eyes2drive viewer/SecondViewController.swift
@@ -27,9 +27,22 @@ class SecondViewController: UIViewController{
             eyeHandler.addEvent(EventType.green)
         };
         
-        
+    }
+    
+    
+    @IBAction func fireStartStop(sender: AnyObject) {
+        switch sender.selectedSegmentIndex{
+        case 0:
+            eyeHandler.startTrip()
+        case 1:
+            eyeHandler.endTrip()
+        default:
+            eyeHandler.startTrip()
+        };
         
     }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
