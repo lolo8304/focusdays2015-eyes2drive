bc18b7f58f6e68221960089b93c1ff32f8a2ab80
Michael Spoerri <michael.spoerri@codingfarm.org>
2015-09-10 14:06:51 +0200

    first protocol

---
+++ eyes2drive viewer/eyes2drive viewer/EyeEventHandlerProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  EyeEventHandlerProtocol.swift
+//  eyes2drive viewer
+//
+//  Created by Michael Spoerri on 10.09.15.
+//  Copyright (c) 2015 Focusdays2015. All rights reserved.
+//
+
+import Foundation
+
+protocol EyeEventHandlerProtocol{
+    
+    func addEvent(type: EventType)
+    func startTrip()
+    func endTrip()
+    
+   }
