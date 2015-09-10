7a7715d585ad2b466f53143d1fed0940c424034c
Rémy Schumm <dev@schumm.ch>
2015-09-11 00:57:57 +0200

    ObjC Header eingebaut

--- eyes2drive viewer/eyes2drive viewer/eyes2drive viewer-Bridging-Header.h
+++ eyes2drive viewer/eyes2drive viewer/eyes2drive viewer-Bridging-Header.h
@@ -2,3 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+// https://developer.apple.com/library/ios/documentation/Swift/Conceptual/BuildingCocoaApps/MixandMatch.html
+
+#import "TransferService.h" 
+#import "BTLECentral.h"
